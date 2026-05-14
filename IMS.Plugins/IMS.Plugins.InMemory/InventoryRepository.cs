47d72b97c7f28c91d3ea9fb356de853ff3ed5a26
Kartik Sharma <kartiksharma4418@gmail.com>
2026-05-15 00:19:36 +0530

    Add project files.

---
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private readonly List<Inventory> _inventory;
+
+        public InventoryRepository()
+        {
+            _inventory = new List<Inventory>()
+            {
+                new Inventory{InventoryId =1,InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
+                new Inventory{InventoryId =2,InventoryName = "Bike Body", Quantity = 10, Price = 15 },
+                new Inventory{InventoryId =3,InventoryName = "Bike Wheels", Quantity = 20, Price = 8 },
+                new Inventory{InventoryId =4,InventoryName = "Bike pedals", Quantity = 20, Price = 1 }
+            };
+        }
+
+        public Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            IEnumerable<Inventory> result;
+            if (string.IsNullOrWhiteSpace(name))
+                result = _inventory;
+            else
+                result = _inventory.Where(x => x.InventoryName != null && x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ IMS.UseCases/Inventories/ViewInventoriesUseCase.cs
@@ -0,0 +1,24 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+
+        }
+
+    }
+}
